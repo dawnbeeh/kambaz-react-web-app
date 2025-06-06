ede7e310291f8ead611af778c9684fae4033a968
Dawnbee <dawnbeeh23@gmail.com>
2025-06-06 00:58:29 -0400

    final fixed

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -22,7 +22,7 @@ export default function TOC() {
         <Nav.Link to="/Kambaz" as={Link}>Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/son-engr-kr/NEU_25SU_CS5610_web_dev" id="wd-github">GitHub repository</Nav.Link>
+        <Nav.Link href="https://github.com/dawnbeeh/kambaz-react-web-app" id="wd-github">GitHub repository</Nav.Link>
       </Nav.Item>
     </Nav>
   );
